286372bd3c5e49176d5f5e8e52e2a4db3d528978
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2023-02-09 19:20:26 -0500

    const client id, secret, redirect uri

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
-console.log(process.env.CLIENT_ID);
 
 const express = require('express');
 const app = express();
 const port = 8888;
 
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
