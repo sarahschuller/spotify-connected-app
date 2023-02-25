a3b98f3a3148a7e40c2f4b9277808bb91649c3ee
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2023-02-25 16:02:22 -0500

    import useEffect, add useEffect hook to App function

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const querystring = window.location.search;
+    const urlParams = new URLSearchParams(querystring);
+    const accessToken = urlParams.get('access_token');
+    const refreshToken = urlParams.get('refresh_token');
+
+    console.log(accessToken);
+    console.log(refreshToken);
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
