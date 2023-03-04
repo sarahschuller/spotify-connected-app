d7eb3f8115681788eb0bfb8113105cc21b04c032
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2023-03-04 18:54:05 -0500

    create spotify.js and add accessToken function

---
+++ client/src/spotify.js
@@ -0,0 +1,8 @@
+const getAccessToken = () => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const accessToken = urlParams.get('access_token');
+    return accessToken;
+  };
+  
+  export const accessToken = getAccessToken();
