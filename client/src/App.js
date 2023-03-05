1ab46ff0d852c8773dd20bb19dd6073b7087cae0
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2023-03-04 19:00:43 -0500

    create ternary function to check for accessToken and render page accordingly

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { accessToken, logout } from './spotify';
 
 function App() {
+
+  const [token, setToken] = useState(null);
+
   useEffect(() => {
-    const querystring = window.location.search;
-    const urlParams = new URLSearchParams(querystring);
-    const accessToken = urlParams.get('access_token');
-    const refreshToken = urlParams.get('refresh_token');
-
-    console.log(accessToken);
-    console.log(refreshToken);
-
-    if (refreshToken) {
-      fetch(`/refresh_token?refresh_token=${refreshToken}`)
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => console.error(err));
-    }
+    setToken(accessToken);
   }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="http://localhost:8888/login"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Log in to Spotify        
-        </a>
+        {!token ? (
+          <a className="App-link" href="http://localhost:8888/login">
+            Log in to Spotify
+          </a>
+        ) : (
+          <h1>Logged in!</h1>
+        )}
       </header>
     </div>
   );
