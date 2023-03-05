dfb61695bfbb64877efbe47b3ef7367954855493
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2023-03-05 15:51:04 -0500

    trouble shooting

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
-import './App.css';
 import { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import { accessToken, logout, getCurrentUserProfile } from './spotify';
 import { catchErrors } from './utils';
+import './App.css';
 
-    function App() {
-      const [token, setToken] = useState(null);
-      const [profile, setProfile] = useState(null);
-    
-      useEffect(() => {
-        setToken(accessToken);
-    
-        const fetchData = async () => {
-            const { data } = await getCurrentUserProfile();
-            setProfile(data);
-        };
+function App() {
+  const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
 
-        catchErrors(fetchData());
-      }, []);
+  useEffect(() => {
+    setToken(accessToken);
+
+    const fetchData = async () => {
+      const { data } = await getCurrentUserProfile();
+      setProfile(data);
+    };
+
+    catchErrors(fetchData());
+  }, []);
 
   return (
     <div className="App">
@@ -26,19 +31,37 @@ import { catchErrors } from './utils';
             Log in to Spotify
           </a>
         ) : (
-          <>
-            <button onClick={logout}>Log Out</button>
+          <Router>
+            <Switch>
+              <Route path="/top-artists">
+                <h1>Top Artists</h1>
+              </Route>
+              <Route path="/top-tracks">
+                <h1>Top Tracks</h1>
+              </Route>
+              <Route path="/playlists/:id">
+                <h1>Playlist</h1>
+              </Route>
+              <Route path="/playlists">
+                <h1>Playlists</h1>
+              </Route>
+              <Route path="/">
+                <>
+                  <button onClick={logout}>Log Out</button>
 
-            {profile && (
-              <div>
-                <h1>{profile.display_name}</h1>
-                <p>{profile.followers.total} Followers</p>
-                {profile.images.length && profile.images[0].url && (
-                  <img src={profile.images[0].url} alt="Avatar"/>
-                )}
-              </div>
-            )}
-          </>
+                  {profile && (
+                    <div>
+                      <h1>{profile.display_name}</h1>
+                      <p>{profile.followers.total} Followers</p>
+                      {profile.images.length && profile.images[0].url && (
+                        <img src={profile.images[0].url} alt="Avatar"/>
+                      )}
+                    </div>
+                  )}
+                </>
+              </Route>
+            </Switch>
+          </Router>
         )}
       </header>
     </div>
