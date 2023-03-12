3033a46aba3dda1d417a07f62a995f3956a72943
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2023-03-11 19:28:02 -0500

    update route for top-artists in App.js, create TopArtists page and export from pages index

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import {
 } from 'react-router-dom';
 import { accessToken, logout } from './spotify';
 import { GlobalStyle } from './styles';
-import { Login, Profile } from './pages';
+import { Login, Profile, TopArtists } from './pages';
 import styled from 'styled-components';
 
 const StyledLogoutButton = styled.button`
@@ -60,7 +60,7 @@ function App() {
             
             <Switch>
               <Route path="/top-artists">
-                <h1>Top Artists</h1>
+                <TopArtists />
               </Route>
               <Route path="/top-tracks">
                 <h1>Top Tracks</h1>
